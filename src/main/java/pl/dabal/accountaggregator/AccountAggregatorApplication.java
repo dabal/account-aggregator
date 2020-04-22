8d8492948fd0f0967d5926102902ecc6c84bbc66
dabal <dabrowski.grzegorz@o2.pl>
2020-04-22 14:48:30 +0200

    initial commit

---
+++ src/main/java/pl/dabal/accountaggregator/AccountAggregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.dabal.accountaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountAggregatorApplication.class, args);
+    }
+
+}
