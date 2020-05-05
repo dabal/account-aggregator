7107e2e5188f919490cbd95142bea613eec87c4c
dabal <dabrowski.grzegorz@o2.pl>
2020-05-05 18:23:26 +0200

    changing sampke data in sql file

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
-insert into users (email, first_name, last_name, password, token)values('test@test.pl', 'Jan', 'Kowalski','moje123haslo','b420ca21-44f3-49d9-9ce1-9a7640e4bdfc');
+insert into users (email, first_name, last_name, password, token)values('test@test.pl', 'Jan', 'Kowalski','$2a$10$5kAj3LNwLG3HQbOqN.6Wju3GPf9Rjjqt5yFsUHL3uXx8eYLBNjYFO','b420ca21-44f3-49d9-9ce1-9a7640e4bdfc');
 INSERT INTO consents (id,created_date,name,scope_time_limit,state,user_id) VALUES (1,'2020-05-05 07:40:28','b0f968ec-8ea3-11ea-98b6-e544f33b4fc5','2020-08-02 07:40:28','b0f968ed-8ea3-11ea-98b6-b56df9e5f956',1);
